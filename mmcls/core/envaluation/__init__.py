a7d9df0342163c5f29d59c817e6072a196f48d47
zlx <2547982531@qq.com>
2021-08-26 16:12:42 +0800

    add the precision_recall_f1 fun

--- mmcls/core/envaluation/__init__.py
+++ mmcls/core/envaluation/__init__.py
@@ -1,3 +1,3 @@
-from .eval_metrics import support,calculate_confusion_matrix
+from .eval_metrics import support,calculate_confusion_matrix,precision_recall_f1
 
-__all__ = ['support','calculate_confusion_matrix']
+__all__ = ['support','calculate_confusion_matrix','precision_recall_f1']
--- mmcls/core/envaluation/eval_metrics.py
+++ mmcls/core/envaluation/eval_metrics.py
@@ -40,3 +40,52 @@ def support(pred, target,average_mode='macro'):
         
     return res
 
+def precision_recall_f1(pred,target,average_mode='macro',thrs = 0.):
+    allowed_average_mode = ['macro','none']
+
+    if average_mode not in allowed_average_mode:
+        raise ValueError(f'Unsuporrt type of averaging {average_mode}.')
+
+    if isinstance(pred,torch.Tensor):
+        pred = pred.numpy()
+    if isinstance(target,torch.Tensor):
+        target = target.numpy()
+
+    if isinstance(thrs, Number):
+        thrs = (thrs, )
+        return_single = True
+    elif isinstance(thrs, tuple):
+        return_single = False
+    else:
+        raise TypeError(
+            f'thrs should be a number or tuple, but got {type(thrs)}.')
+
+    label = np.indices(pred.shape)[1]
+    #取最大的值的label
+    pred_label = np.argsort(pred,axis=1)[:,-1]
+    pred_socre = np.sort(pred,axis=1)[:,-1]
+
+    precisions = []
+    recalls = []
+    f1_scores = []
+    for thr in thrs:
+        _pred_label = pred_label.copy
+        if thr is not None:
+            _pred_label[pred_socre<=thr] = -1#概率小于thr的不会属于那一类
+        pred_positive = label == _pred_label.reshape(-1,1)
+        gt_positive = label == target.reshape(-1,1)
+        precision = (pred_positive & gt_positive).sum(0)/np.maximum(pred_positive.sum(0),1)*100
+        recall = (pred_positive & gt_positive).sum(0) / np.maximum(gt_positive.sum(0), 1) * 100
+        f1_score = 2 * precision * recall / np.maximum(precision + recall, 1e-20)
+        if average_mode == 'macro':
+            precision = float(precision.mean())
+            recall = float(recall.mean())
+            f1_score = float(f1_score.mean())
+        precisions.append(precision)
+        recalls.append(recall)
+        f1_scores.append(f1_score)
+
+    if return_single:
+        return precisions[0], recalls[0], f1_scores[0]
+    else:
+        return precisions, recalls, f1_scores
