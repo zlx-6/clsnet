1dabe52f94bcc25636aa80416d8acb2929d5063c
zlx <2547982531@qq.com>
2021-08-26 14:34:42 +0800

    add the compose funcation in pipline

---
+++ mmcls/datasets/pipelines/compose.py
@@ -0,0 +1,33 @@
+from mmcls.cvcore import build_from_cfg
+from mmcls.datasets.builder import PIPELINES
+
+@PIPELINES.register_module()
+class Compose(object):
+
+    def __init__(self,transforms):
+        self.transforms = []
+        if transforms is not None:
+            for transform in transforms:
+                if isinstance(transform,dict):
+                    build_from_cfg(transform,PIPELINES)#第二个参数是从哪个Register里面取dict_modules，Register下会注册许多modules
+                    self.transforms.append(transform)
+                elif callable(transform):
+                    self.transforms.append(transform)
+
+
+    def call(self,data):
+        for t in self.transforms:
+            data = t(data)
+            if data is None:
+                return None
+        return data
+
+    def __repr__(self):
+        format_string = self.__class__.__name__ + '('
+        for t in self.transforms:
+            format_string +=f'\n {t}'
+        format_string += '\n)'
+        return format_string#打印用的
+
+if __name__ == "__main__":
+    print(Compose(None))
