423009509e6e7f77aa3ed265a2bf2a6abd977784
zlx <2547982531@qq.com>
2021-08-27 15:10:45 +0800

    add the first pipeline: RandomCrop

---
+++ mmcls/cvcore/image/geometric.py
@@ -0,0 +1,97 @@
+import cv2
+import numbers
+import numpy as np
+
+def bbox_clip(bboxes,img_shape):
+    cmin = np.empty(bboxes.shape[-1],dtype=bboxes.dtype)
+    cmin[0::2] = img_shape[1] - 1
+    cmin[1::2] = img_shape[0] - 1
+    clipped_bboxes = np.maximum(np.minimum(bboxes, cmin), 0)
+    return clipped_bboxes
+
+def bbox_scaling(bboxes,scale,clip_shape=None):
+    if float(scale) == 1.0:
+        scaled_bboxes = bboxes.copy()
+    else:
+        w = bboxes[..., 2] - bboxes[..., 0] + 1
+        h = bboxes[..., 3] - bboxes[..., 1] + 1
+        dw = (w * (scale - 1)) * 0.5
+        dh = (h * (scale - 1)) * 0.5
+        scaled_bboxes = bboxes + np.stack((-dw, -dh, dw, dh), axis=-1)
+    if clip_shape is not None:
+        return bbox_clip(scaled_bboxes, clip_shape)
+    else:
+        return scaled_bboxes
+
+
+def imcrop(img,bboxes,scales = 1.0 ,pad_fill = None):
+    chn = 1 if img.ndim == 2 else img.shape[2]
+    if pad_fill is not None:
+        if isinstance(pad_fill,(int,float)):
+            pad_fill = [pad_fill for _ in range(chn)]
+    
+    _bboxes = bboxes[None,...] if bboxes.ndim == 1 else bboxes#array([[ 4,  0, 35, 31]])
+    scaled_bboxes = bbox_scaling(_bboxes,scales).astype(np.int32)
+    clipped_bboxes = bbox_clip(scaled_bboxes,img.shape)#这俩函数可以合并吧array([ 7,  8, 38, 39])
+    
+    patches =[]
+    for i in range(clipped_bboxes.shape[0]):
+        x1, y1, x2, y2 = tuple(clipped_bboxes[i,:])
+        if pad_fill is None:
+            patch = img[y1:y2+1,x1:x2+1,...]
+        else:
+            _x1, _y1, _x2, _y2 = tuple(scaled_bboxes[i, :])
+            if chn == 1:
+                patch_shape = (_y2 - _y1 + 1, _x2 - _x1 + 1)
+            else:
+                patch_shape = (_y2 - _y1 + 1, _x2 - _x1 + 1, chn)
+            patch = np.array(
+                pad_fill, dtype=img.dtype) * np.ones(
+                    patch_shape, dtype=img.dtype)
+            x_start = 0 if _x1 >= 0 else -_x1
+            y_start = 0 if _y1 >= 0 else -_y1
+            w = x2 - x1 + 1
+            h = y2 - y1 + 1
+            patch[y_start:y_start + h, x_start:x_start + w,
+                  ...] = img[y1:y1 + h, x1:x1 + w, ...]
+        patches.append(patch)
+
+    if bboxes.ndim == 1:
+        return patches[0]
+    else:
+        return patches
+
+
+def impad(img,*,shape=None,padding=None,pad_val=0,padding_mode='constant'):
+    assert (shape is not None) ^ (padding is not None)
+    if shape is not None:
+        padding = (0,0,shape[1]-img.shape[1],shape[0]-img.shape[0])
+    
+    if isinstance(pad_val,tuple):
+        assert len(pad_val) == img.shape[-1]#每个通道吗？？
+
+    if isinstance(padding,tuple) and len(padding) in [2,4]:
+        if len(padding)==2:
+            padding == (padding[0], padding[1], padding[0], padding[1])
+    elif isinstance(padding, numbers.Number):#检查是不是一个数,包括浮点数和小数
+        padding = (padding, padding, padding, padding)#(4, 4, 4, 4)
+    else:
+        raise ValueError('Padding must be a int or a 2, or 4 element tuple.'
+                         f'But received {padding}')
+    
+    assert padding_mode in ['constant', 'edge', 'reflect', 'symmetric']
+
+    border_type = {
+        'constant': cv2.BORDER_CONSTANT,
+        'edge': cv2.BORDER_REPLICATE,
+        'reflect': cv2.BORDER_REFLECT_101,
+        'symmetric': cv2.BORDER_REFLECT
+    }
+    img = cv2.copyMakeBorder(img,padding[1],
+        padding[3],
+        padding[0],
+        padding[2],
+        border_type[padding_mode],
+        value=pad_val)#(32, 32, 3)-->(40, 40, 3)
+
+    return img
---
+++ mmcls/datasets/pipelines/transforms.py
@@ -0,0 +1,64 @@
+from mmcv.image.geometric import imcrop
+from mmcls.datasets.builder import PIPELINES
+from mmcls.cvcore.image import impad,imcrop
+
+import numpy as np
+import random
+#import mmcv
+
+@PIPELINES.register_module()
+class RandomCrop(object):
+
+    def __init__(self,size,padding=None,pad_if_needed=False,pad_val=0,padding_module ='constant'):
+        if isinstance(size,(tuple,list)):
+            self.size = size
+        else:
+            self.size = (size,size)#32
+        self.padding = padding #4
+        self.pad_if_needed = pad_if_needed
+        self.pad_val = pad_val#0
+        self.padding_module = padding_module
+
+    @staticmethod
+    def get_params(img,output_size):
+
+        height = img.shape[0]
+        width = img.shape[1]
+        target_height, target_width = output_size
+        if width == target_width and height == target_height:
+            return 0, 0, height, width
+
+        ymin = random.randint(0, height - target_height)
+        xmin = random.randint(0, width - target_width)
+        return ymin, xmin, target_height, target_width
+
+
+    def __call__(self,results):
+        for key in results.get('img_fileds',['img']):
+            img = results[key]
+            if self.padding is not None:
+                img = impad(img,padding = self.padding,padding_mode=self.padding_module)
+            #填充高度
+            if self.pad_if_needed and img.shape[0]<self.size[0]:
+                img = impad(img,padding=(0, self.size[0] - img.shape[0], 0,#左上右下模式,这个填充模式有错吗？？？
+                             self.size[0] - img.shape[0]),pad_val=self.pad_val,
+                    padding_mode=self.padding_mode)
+
+            # pad the width if needed
+            if self.pad_if_needed and img.shape[1] < self.size[1]:
+                img = impad(
+                    img,
+                    padding=(self.size[1] - img.shape[1], 0,
+                             self.size[1] - img.shape[1], 0),
+                    pad_val=self.pad_val,
+                    padding_mode=self.padding_mode)
+
+            ymin,xmin,height,width = self.get_params(img,self.size)#得到随机剪裁的目标#0,4,32,32
+            results[key] = imcrop(img,np.array([
+                    xmin,
+                    ymin,
+                    xmin + width - 1,
+                    ymin + height - 1,
+                ]))
+        return results
+        
