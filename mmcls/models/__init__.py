a334f9a3f6a66138c3164fc0ed85b49342c00753
zlx <2547982531@qq.com>
2021-08-28 15:56:32 +0800

    add the ce loss

--- mmcls/models/__init__.py
+++ mmcls/models/__init__.py
@@ -5,7 +5,8 @@ from .classifiers import ImageClassifier
 from .backbone import MobileNetV3
 from .heads import ClsHead,StackedLinearClsHead
 from .necks import GlobalAveragePooling
+from .loss import CrossEntropyLoss
 
 __all__ = ['build_classifier','build_backbone','build_head','build_loss','build_neck',
             'BACKBONES','NECKS','HEADS','CLASSIFIERS','ImageClassifier','ClsHead','StackedLinearClsHead',
-            'GlobalAveragePooling','MobileNetV3']
+            'GlobalAveragePooling','MobileNetV3','CrossEntropyLoss']
--- mmcls/models/loss/__init__.py
+++ mmcls/models/loss/__init__.py
@@ -1,3 +1,5 @@
 from .accuracy import accuracy,Accuracy
+from .utils import weight_reduce_loss
+from .cross_entropy_loss import CrossEntropyLoss
 
-__all__ = ['accuracy','Accuracy']
+__all__ = ['accuracy','Accuracy','weight_reduce_loss','CrossEntropyLoss']
