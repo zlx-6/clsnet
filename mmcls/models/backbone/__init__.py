5e40dd19ea947a0d63722dc6e42ff134007920d8
zlx <2547982531@qq.com>
2021-08-31 19:05:24 +0800

    add resnet18 cifar

--- mmcls/models/backbone/__init__.py
+++ mmcls/models/backbone/__init__.py
@@ -1,4 +1,6 @@
 from .base_backbone import BaseBackbone
 from .mobilenet_v3 import MobileNetV3
+from .resnet import ResNet,ResNetV1d
+from .resnet_cifar import ResNet_CIFAR
 
-__all__ = ['BaseBackbone','MobileNetV3']
+__all__ = ['BaseBackbone','MobileNetV3','ResNet','ResNetV1d','ResNet_CIFAR']
--- mmcls/models/heads/__init__.py
+++ mmcls/models/heads/__init__.py
@@ -1,5 +1,6 @@
 from .base_head import BaseHead
 from .cls_head import ClsHead
 from .stacked_head import StackedLinearClsHead
+from .liner_head import LinearClsHead
 
-__all__ = ['BaseHead','ClsHead','StackedLinearClsHead']
+__all__ = ['BaseHead','ClsHead','StackedLinearClsHead','LinearClsHead']
